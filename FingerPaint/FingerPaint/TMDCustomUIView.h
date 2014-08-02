5a48cf0aab7c11e23020d71f72e0d7153d9ec6d0
Tom Daggett <tomd@pobox.com>
2014-08-01 17:22:24 -0700

    added custom UIView

---
+++ FingerPaint/FingerPaint/TMDCustomUIView.h
@@ -0,0 +1,13 @@
+//
+//  TMDCustomUIView.h
+//  FingerPaint
+//
+//  Created by Tom Daggett on 2014-08-01.
+//  Copyright (c) 2014 Tom Daggett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMDCustomUIView : UIView
+
+@end
---
+++ FingerPaint/FingerPaint/TMDCustomUIView.m
@@ -0,0 +1,13 @@
+//
+//  TMDCustomUIView.m
+//  FingerPaint
+//
+//  Created by Tom Daggett on 2014-08-01.
+//  Copyright (c) 2014 Tom Daggett. All rights reserved.
+//
+
+#import "TMDCustomUIView.h"
+
+@implementation TMDCustomUIView
+
+@end
